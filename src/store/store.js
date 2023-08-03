072570993ca9280538ac10163a1fe64fd6f7e926
akbrsazish <sazishaliakbar@gmail.com>
2023-08-04 02:17:37 +0330

    add reducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { useReducer } from "react";
 
 const store = configureStore ({
     reducer: {
-      
+        user: useReducer
     }
 });
 
